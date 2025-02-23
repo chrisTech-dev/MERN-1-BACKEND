6f70a1ffc4a4d23e40241d78f6f60a1db73955f4
watsonivy <christopheraveson@gmail.com>
2025-02-23 13:42:08 +0000

    Updated feature

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    const con = await mongoose.connect("mongodb://127.0.0.1:27017/products");
+    const con = await mongoose.connect(process.env.MONGO_URL);
     console.log(`✅ MongoDB connected successfully: ${con.connection.host}`);
   } catch (error) {
     console.error(`❌ Error connecting to MongoDB: ${error.message}`);
